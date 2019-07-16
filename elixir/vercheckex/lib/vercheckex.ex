1eb7aa72fec91c1e6515eb547892a2eaf6550276
Takamichi Omori <merrytakamerry@gmail.com>
2019-07-16 23:40:16 +0900

    test

---
+++ elixir/vercheckex/lib/vercheckex.ex
@@ -0,0 +1,18 @@
+defmodule Vercheckex do
+  @moduledoc """
+  Documentation for Vercheckex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Vercheckex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir/vercheckex/test/vercheckex_test.exs
@@ -0,0 +1,8 @@
+defmodule VercheckexTest do
+  use ExUnit.Case
+  doctest Vercheckex
+
+  test "greets the world" do
+    assert Vercheckex.hello() == :world
+  end
+end
