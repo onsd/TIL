49aea0fcf5a9615ce95bd4905a8b216c43c2d531
Takamichi Omori <merrytakamerry@gmail.com>
2020-06-10 16:36:23 +0900

    add script

--- deploy.bat
+++ deploy.bat
@@ -12,12 +12,7 @@
 @ if not exist "%QUARTUS_BIN%" set QUARTUS_BIN=%QUARTUS_ROOTDIR%\bin64
 
 
-%QUARTUS_BIN%\\quartus_map --read_settings_files=on --write_settings_files=off DE10_LITE_Default -c DE10_LITE_Default
-%QUARTUS_BIN%\\quartus_fit --read_settings_files=off --write_settings_files=off DE10_LITE_Default -c DE10_LITE_Default
-%QUARTUS_BIN%\\quartus_asm --read_settings_files=off --write_settings_files=off DE10_LITE_Default -c DE10_LITE_Default
-%QUARTUS_BIN%\\quartus_sta DE10_LITE_Default -c DE10_LITE_Default
-
-%QUARTUS_BIN%\\quartus_pgm.exe -m jtag -c 1 -o "p;DE10_LITE_Default.sof"
+%QUARTUS_BIN%\\quartus_map --read_settings_files=on --write_settings_files=off DE10_LITE_Default -c DE10_LITE_Default && %QUARTUS_BIN%\\quartus_fit --read_settings_files=off --write_settings_files=off DE10_LITE_Default -c DE10_LITE_Default && %QUARTUS_BIN%\\quartus_asm --read_settings_files=off --write_settings_files=off DE10_LITE_Default -c DE10_LITE_Default && %QUARTUS_BIN%\\quartus_sta DE10_LITE_Default -c DE10_LITE_Default && %QUARTUS_BIN%\\quartus_pgm.exe -m jtag -c 1 -o "p;DE10_LITE_Default.sof"
 @ set SOPC_BUILDER_PATH=%SOPC_KIT_NIOS2%+%SOPC_BUILDER_PATH%
 
 pause
