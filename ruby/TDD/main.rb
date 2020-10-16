d40ec1c512578e191f9961eea697150c5d5fadfa
Takamichi Omori <merrytakamerry@gmail.com>
2020-10-16 22:44:41 +0900

    return number as string

--- ruby/TDD/main.rb
+++ ruby/TDD/main.rb
@@ -5,11 +5,27 @@ require 'minitest/autorun'
 class FizzBuzzTest < Minitest::Test
   def test_1を渡したら文字列1を返す
     assert_equal '1', FizzBuzz.generate(1)
+    assert_equal '2', FizzBuzz.generate(2)
+    assert_equal '3', FizzBuzz.generate(3)
+    assert_equal '4', FizzBuzz.generate(4)
+    assert_equal '5', FizzBuzz.generate(5)
+
+
+
   end
 end
 
 class FizzBuzz
     def self.generate(n)
-        '1'
+        n.to_s
     end
 end
+
+# TODO
+# 数を文字列にして返す
+# 1を渡したら文字列"1"を返す
+# 3 の倍数のときは数の代わりに｢Fizz｣と返す
+# 5 の倍数のときは｢Buzz｣と返す
+# 3 と 5 両方の倍数の場合には｢FizzBuzz｣と返す
+# 1 から 100 までの数
+# プリントする
