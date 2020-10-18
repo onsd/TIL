5b26478daa87c03fe4e5b1c245978a37ce3c8153
Takamichi Omori <merrytakamerry@gmail.com>
2020-10-18 21:30:21 +0900

    add ruby

--- ruby/TDD/main.rb
+++ ruby/TDD/main.rb
@@ -6,19 +6,25 @@ class FizzBuzzTest < Minitest::Test
     def setup
         @fizzbuzz = FizzBuzz
     end
-    def test_1を渡したら文字列1を返す
+    def numberを渡したらFizzbuzzをやる
         assert_equal '1', @fizzbuzz.generate(1)
         assert_equal '2', @fizzbuzz.generate(2)
         assert_equal 'Fizz', @fizzbuzz.generate(3)
         assert_equal '4', @fizzbuzz.generate(4)
-        assert_equal '5', @fizzbuzz.generate(5)
+        assert_equal 'Buzz', @fizzbuzz.generate(5)
+        assert_equal 'FizzBuzz', @fizzbuzz.generate(15)
+
     end
 end
 
 class FizzBuzz
     def self.generate(number)
-        if number % 3 == 0
-           return 'Fizz'
+        if number.modulo(3).zero? && number.modulo(5).zero?
+            return 'FizzBuzz'
+        elsif number.modulo(3).zero?
+            return 'Fizz'
+        elsif number.modulo(5).zero?
+            return 'Buzz'
         end
         return number.to_s
     end
