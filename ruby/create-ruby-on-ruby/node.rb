68112721fbcf129b8ed7e84b93c0875d6584f2ab
Takamichi Omori <merrytakamerry@gmail.com>
2021-07-06 17:09:20 +0900

    dai-3-kai

--- ruby/create-ruby-on-ruby/node.rb
+++ ruby/create-ruby-on-ruby/node.rb
@@ -5,11 +5,9 @@ leaf_c = ["葉C"]
 leaf_d = ["葉D"]
 
 # 節
-node2 = ["節2", leaf_a, leaf_b]
-node3 = ["節3", leaf_c, leaf_d]
-
-# 木
-node1 = ["節1", node2, node3]
+node3 = ["節3", leaf_b, leaf_d]
+node2 = ["節2", leaf_a, node3]
+node1 = ["節1", node2, leaf_d]
 
 
 def preorder(tree)
@@ -20,4 +18,37 @@ def preorder(tree)
     end
 end
 
+def preorder_only_leaf(tree)
+    if tree[1].nil?
+        p(tree[0])
+    end
+    if tree[0].start_with?("節")
+        preorder_only_leaf(tree[1])
+        preorder_only_leaf(tree[2])
+    end
+end
+
+def postorder(tree)
+    if tree[0].start_with?("節")
+        postorder(tree[1])
+        postorder(tree[2])
+    end
+    p(tree[0])
+end
+
 preorder(node1)
+p("====")
+
+preorder_only_leaf(node1)
+p("====")
+
+postorder(node1)
+p("====")
+
+# 節
+node2 = ["節2", leaf_a, leaf_b]
+node3 = ["節3", leaf_c, leaf_d]
+
+# 木
+node1 = ["節1", node2, node3]
+postorder(node1)
